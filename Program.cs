60b40d64b0a10265673210f3bf69f5307fc68ab2
PietjeGeluk <62058850+PietjeGeluk@users.noreply.github.com>
2020-03-17 13:03:31 +0100

    Add files via upload
    Constructor zonder parameters

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp38
+{
+    class Program
+    {
+
+        class Car 
+        {
+            public string model;
+            public Car()
+            {
+                model = "Passat";
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Car Volkswagen = new Car();
+            Console.WriteLine(Volkswagen.model);
+        }
+
+
+    }
+}
